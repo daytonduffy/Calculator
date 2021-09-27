b23bca389ade826d9cbdca3c46eedc1cc26fb507
Dayton Duffy <drduffy@wisc.edu>
2021-09-27 15:13:57 -0500

    DA Check

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    // four seperate calls
     public void clickFunctionAdd(View view) {
         EditText integerOne = (EditText) findViewById(R.id.integerOne);
         int i1 = Integer.parseInt(integerOne.getText().toString());
